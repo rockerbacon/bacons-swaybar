d1fc21abbb556c40707092fe896bbd1d1c5514e3
rockerbacon <vitor.santos@lab309.com.br>
2024-06-07 13:47:56 -0300

    improve and simplify battery bar display

--- src/battery/mod.rs
+++ src/battery/mod.rs
@@ -1,4 +1,3 @@
-use std::cmp;
 use std::env;
 use std::fmt;
 
@@ -6,46 +5,51 @@ use crate::common::icon;
 use crate::common::Widget;
 use crate::common::Sysfs;
 
-const BATTERY_ANIM: [char; 2] = [icon::SQR_S, icon::SQR_L];
-
 pub struct Battery {
-	bar: [char; 3],
-	pct: u8,
+	anim_cycle: u8,
+	bar: u8,
 	max_charge: u8,
-	clock: usize,
 	sysfs: Sysfs,
 	plugged_in: bool,
 }
 
 impl Battery {
-	fn update_pct(&mut self) {
-		let raw = self.sysfs.get::<u8>("BAT0", "capacity");
-		let pct: f32 = raw as f32 / self.max_charge as f32 * 100f32;
-
-		self.pct = cmp::min(100, pct as u8);
+	fn get_pct(&self) -> u8 {
+		return self.sysfs.get::<u8>("BAT0", "capacity");
 	}
 
 	fn update_plugged_in(&mut self) {
 		self.plugged_in = self.sysfs.get::<char>("AC", "online") == '1';
 	}
 
-	fn charge_low(&mut self) {
-		self.bar[0] = BATTERY_ANIM[self.clock % 2];
-		self.clock += 1;
+	fn update_bar(&mut self, pct: u8) {
+		match pct {
+			(0..=15) => self.bar = 0,
+			(16..=33) => self.bar = 0b001,
+			(34..=65) => self.bar = 0b011,
+			(66..=100) => self.bar = 0b111,
+			_ => (),
+		};
+
+		self.anim_cycle = 0;
 	}
 
-	fn charge_mid(&mut self, range: usize) {
-		self.bar[range-1] = icon::SQR_L;
-		self.bar[range] = BATTERY_ANIM[self.clock % 2];
-		self.clock += 1;
+	fn animate_bar(&mut self) {
+		if self.anim_cycle == 0 {
+			self.bar >>= 1;
+		} else {
+			self.bar = (self.bar << 1) + 1;
+		}
+
+		self.anim_cycle ^= 1;
 	}
 
-	fn charge_full(&mut self) {
-		self.bar[2] = icon::SQR_L;
-	}
+	fn bar_icon(&self, bit: u8) -> char {
+		if (bit & self.bar) != 0 {
+			return icon::SQR_L;
+		}
 
-	fn discharge(&mut self, level: usize) {
-		self.bar[level] = icon::SQR_S;
+		return icon::SQR_S;
 	}
 
 	pub fn new() -> Battery {
@@ -55,10 +59,9 @@ impl Battery {
 		};
 
 		let mut bat = Battery {
-			bar: ['\0'; 3],
-			pct: 0,
+			anim_cycle: 0,
+			bar: 0,
 			max_charge,
-			clock: 0,
 			sysfs: Sysfs::new("power_supply"),
 			plugged_in: false,
 		};
@@ -66,15 +69,8 @@ impl Battery {
 		bat.sysfs.watch("BAT0", "capacity");
 		bat.sysfs.watch("AC", "online");
 
-		bat.update_pct();
 		bat.update_plugged_in();
-
-		bat.bar = match bat.pct {
-			90..=100 => [icon::SQR_L, icon::SQR_L, icon::SQR_L],
-			46..=89 => [icon::SQR_L, icon::SQR_L, icon::SQR_S],
-			16..=45 => [icon::SQR_L, icon::SQR_S, icon::SQR_S],
-			_ => [icon::SQR_S, icon::SQR_S, icon::SQR_S],
-		};
+		bat.update_bar(bat.get_pct());
 
 		return bat;
 	}
@@ -82,57 +78,35 @@ impl Battery {
 
 impl fmt::Display for Battery {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		let main_icn = match (self.plugged_in, self.pct) {
+		let main_icn = match (self.plugged_in, self.bar) {
 			(true, ..) => icon::BOLT,
-			(false, 16..=100) => icon::BATT_HIG,
+			(false, 0b1..=0b111) => icon::BATT_HIG,
 			_ => icon::BATT_LOW,
 		};
 
-		return write!(f, "{} {}{}{}", main_icn, self.bar[0], self.bar[1], self.bar[2]);
+		return write!(f, "{} {}{}{}", main_icn, self.bar_icon(0b1), self.bar_icon(0b10), self.bar_icon(0b100));
 	}
 }
 
 impl Widget for Battery {
 	fn update(&mut self) -> bool {
+		let prev_bar = self.bar;
+		let prev_plugged_in = self.plugged_in;
+
 		let has_updates = self.sysfs.update();
+		let pct = self.get_pct();
 
 		if has_updates {
 			self.update_plugged_in();
-			self.update_pct();
+			self.update_bar(pct);
+		} 
 
-			match (self.plugged_in, self.pct) {
-				(true, 90..=100) => self.charge_full(),
-				(true, 46..=89) => self.charge_mid(2),
-				(true, 16..=45) => self.charge_mid(1),
-				(true, 0..=15) => self.charge_low(),
-				(false, 46..=89) => self.discharge(2),
-				(false, 16..=45) => self.discharge(1),
-				(false, 0..=15) => self.discharge(0),
-				_ => (),
-			};
+		if self.plugged_in && pct < self.max_charge - 5 {
+			self.animate_bar();
+		}
 
-			return true;
-		} else if self.plugged_in {
-			let is_animated = match self.pct {
-				46..=89 => {
-					self.charge_mid(2);
-					return true;
-				},
-				16..=45 => {
-					self.charge_mid(1);
-					return true;
-				},
-				0..=15 => {
-					self.charge_low();
-					return true;
-				},
-				_ => false,
-			};
-
-			return is_animated;
-		} else {
-			return false;
-        }
+		return prev_bar != self.bar ||
+			prev_plugged_in != self.plugged_in;
 	}
 
 	fn on_click(&self) -> Option<&str> {

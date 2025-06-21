a26b8bbe6580fa59ce889b371d40d299c72637c8
rockerbacon <vitor.santos@lab309.com.br>
2025-06-21 17:17:49 -0300

    rewrite entrypoint in C

---
+++ src/main.c
@@ -0,0 +1,49 @@
+#define _POSIX_C_SOURCE 199309L
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <widgets/clock.h>
+
+#define BUFFSIZE 512
+
+int main(void) {
+	char buff[BUFFSIZE];
+	struct wgt* wgts[] = {
+		&wgt_clock,
+	};
+	size_t wgt_count = sizeof(wgts) / sizeof(struct wgt*);
+	struct timespec sleep_duration;
+
+	memset(buff, 0, BUFFSIZE);
+
+	for (size_t i = 0; i < wgt_count; i++) {
+		struct wgt* w = wgts[i];
+		if (w->init != NULL) {
+			w->init();
+		}
+	}
+
+	while(1) {
+		size_t offset = 0;
+		for (size_t i = 0; i < wgt_count; i++) {
+			struct wgt* w = wgts[i];
+			if (w->display != NULL) {
+				offset += w->display(buff+offset, BUFFSIZE-offset);
+			}
+
+			if (offset >= BUFFSIZE) {
+				fprintf(stderr, "FATAL: buffer overflow\n");
+				exit(1);
+			}
+		}
+
+		fprintf(stdout, "%s\n", buff);
+		clk_sync_interval(&sleep_duration);
+		nanosleep(&sleep_duration, NULL);
+	}
+
+	return 0;
+}
